5d64a2e6a039ac74e64fe745fe33fa6063864ad9
Saranga <bsaranga@gmail.com>
2022-08-31 22:26:05 +0530

    testing basic facts

--- DataStructures_Tests/CommonTests.cs
+++ DataStructures_Tests/CommonTests.cs
@@ -1,7 +1,16 @@
-﻿namespace DataStructures_Tests
+﻿using Xunit.Abstractions;
+
+namespace DataStructures_Tests
 {
     public class CommonTests
     {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public CommonTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public void CanExtendArrayByConcatenation()
         {
@@ -22,6 +31,28 @@
             {
                 Assert.True(item == default(int));
             }
+        }
+
+        [Fact]
+        public void ArraysAreReferenceTypes()
+        {
+            int[] arr1 = { 1, 2, 3, 4, 5 };
+
+            var arr2 = arr1;
+            var arr3 = arr1;
+
+            Assert.True(Object.ReferenceEquals(arr2, arr3));
+        }
+
+        [Fact]
+        public void WTFAreReferenceTypes()
+        {
+            string foo = "foo";
+            string bar = foo;
+
+            bar.Append('b');
+
+            testOutputHelper.WriteLine(bar);
         }
     }
 }
