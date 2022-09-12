6391b373ebb26ae7d949f1b930c0f85bc4bb9e2b
bsaranga <bsaranga@gmail.com>
2022-09-12 10:11:32 +0530

    indexed type tests

--- DataStructures/DynamicArrays/IndexedType.cs
+++ DataStructures/DynamicArrays/IndexedType.cs
@@ -5,48 +5,77 @@ namespace DataStructures.DynamicArrays
     public class IndexedType<T> : IEnumerator<T>
     {
         private T[] store;
+        private int position = -1;
+
+        /// <summary>
+        /// Returns the length of the internal storage array
+        /// </summary>
+        public int Length { get { return store.Length; } }
 
         public IndexedType(int size = 5)
         {
-             store = new T[size];
+            store = new T[size];
         }
 
         public T this[int index]
-        { 
-            get { return (T) new object(); }
-            set 
-            { 
+        {
+            get 
+            {
+                try
+                {
+                    return (T)store[index];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw;
+                }
+            }
+            set
+            {
                 if (index < store.Length) store[index] = value;
                 else
                 {
-                    var copy = (T[]) store.Clone();
+                    var copy = (T[])store.Clone();
 
                     store = new T[index + 1];
                     for (int i = 0; i < copy.Length; i++)
                     {
                         store[i] = copy[i];
                     }
+
+                    store[index] = value;
                 }
             }
         }
 
-        public T Current => throw new NotImplementedException();
+        public T Current 
+        { 
+            get 
+            {
+                try
+                {
+                    return store[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw;
+                }
+            }
+        }
 
         object IEnumerator.Current => Current!;
 
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
+        public void Dispose() {}
 
         public bool MoveNext()
         {
-            throw new NotImplementedException();
+            position++;
+            return position < store.Length;
         }
 
         public void Reset()
         {
-            throw new NotImplementedException();
+            position = -1;
         }
     }
 }
---
+++ DataStructures_Tests/IndexedTypeTests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit.Abstractions;
+using DataStructures.DynamicArrays;
+
+namespace DataStructures_Tests
+{
+    public class IndexedTypeTests
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+        public IndexedTypeTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void InitializedArrayOfSizeFive()
+        {
+            var indexedType = new IndexedType<string>();
+            Assert.Equal(5, indexedType.Length);
+        }
+
+        [Fact]
+        public void IndexedArrayIsPopulatedByDefaultValues()
+        {
+            var indexedType = new IndexedType<int>();
+            Assert.Equal(0, indexedType[0]);
+            Assert.Equal(0, indexedType[1]);
+            Assert.Equal(0, indexedType[2]);
+            Assert.Equal(0, indexedType[3]);
+            Assert.Equal(0, indexedType[4]);
+        }
+
+        [Fact]
+        public void IndexedArrayCanResize()
+        {
+            var indexedType = new IndexedType<int>();
+            indexedType[5] = 1;
+            Assert.Equal(6, indexedType.Length);
+        }
+
+        [Fact]
+        public void AccessingOutOfBoundsRaisesException()
+        {
+            var indexedType = new IndexedType<int>();
+            Assert.Throws<IndexOutOfRangeException>(() => indexedType[5]);
+        }
+    }
+}
