a6e633acc1dd849d7950cc2b9f16d262107dcaa4
bsaranga <bsaranga@gmail.com>
2022-09-14 09:50:31 +0530

    repeatedStrings WIP

---
+++ HackerRank/InterviewPreparationKit/WarmupChallenges/RepeatedStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarmupChallenges
+{
+    public class RepeatedStrings
+    {
+        public long repeatedString(string s, long n)
+        {
+            // abaabaabaaba|aba
+            long duplicates = (long) Math.Ceiling((decimal) n/s.Length);
+            string repeatedSubStr = "";
+
+            for (int i = 0; i <= duplicates; i++)
+            {
+                repeatedSubStr += s;
+            }
+
+            var numAs = repeatedSubStr.Substring(0, (int)n).Count(s => s == 'a');
+
+            return numAs;
+        }
+    }
+}
---
+++ HackerRank/InterviewPreparationKit/WarmupChallenges_Tests/RepeatedStrings_Tests.cs
@@ -0,0 +1,16 @@
+﻿using WarmupChallenges;
+
+namespace WarmupChallenges_Tests
+{
+    public class RepeatedStrings_Tests
+    {
+        [Fact]
+        public void RepeatedStrings_Test1()
+        {
+            var r = new RepeatedStrings();
+            var result = r.repeatedString("aba", 10);
+
+            Assert.Equal(7, result);
+        }
+    }
+}
