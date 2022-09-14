9ffeb5d40780711faf4076ddb75972c8404767ea
bsaranga <bsaranga@gmail.com>
2022-09-14 09:19:07 +0530

    Jumping on clouds complete

---
+++ HackerRank/InterviewPreparationKit/WarmupChallenges/JumpingOnClouds.cs
@@ -0,0 +1,29 @@
+﻿namespace WarmupChallenges
+{
+    public class JumpingOnClouds
+    {
+        public int Jump (List<int> c)
+        {
+            int maxJump = 2;
+            int minJump = 1;
+            int jumpCount = 0;
+            int index = 0;
+            int length = c.Count;
+
+            while(index < length - 1)
+            {
+                if ((index + maxJump) < length && c[index + maxJump] == 0)
+                {
+                    jumpCount++;
+                    index += maxJump;
+                } else
+                {
+                    jumpCount++;
+                    index += minJump;
+                }
+            }
+
+            return jumpCount;
+        }
+    }
+}
---
+++ HackerRank/InterviewPreparationKit/WarmupChallenges_Tests/JumpingOnClouds_Tests.cs
@@ -0,0 +1,51 @@
+﻿using WarmupChallenges;
+using Xunit.Abstractions;
+
+namespace WarmupChallenges_Tests
+{
+    public class JumpingOnClouds_Tests
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public JumpingOnClouds_Tests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void JumpingOnClouds_Test1()
+        {
+            var jumpingOnClouds = new JumpingOnClouds();
+            var result = jumpingOnClouds.Jump(new List<int> { 0, 0, 1, 0, 0, 1, 0 });
+
+            Assert.Equal(4, result);
+        }   
+
+        [Fact]
+        public void JumpingOnClouds_Test2()
+        {
+            var jumpingOnClouds = new JumpingOnClouds();
+            var result = jumpingOnClouds.Jump(new List<int> { 0, 0, 0, 0, 1, 0 });
+
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void JumpingOnClouds_Test3()
+        {
+            var jumpingOnClouds = new JumpingOnClouds();
+            var result = jumpingOnClouds.Jump(new List<int> { 0, 0, 0, 1, 0, 0 });
+
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void JumpingOnClouds_Test4()
+        {
+            var jumpingOnClouds = new JumpingOnClouds();
+            var result = jumpingOnClouds.Jump(new List<int> { 0, 0, 1, 0, 0, 1, 0 });
+
+            Assert.Equal(4, result);
+        }
+    }
+}
